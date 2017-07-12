20cfe709eac3e797bcd717ae39532e321e66b44f
Bharat <bharat@knoldus.com>
2017-07-12 13:48:03 +0530

    Added dynamic logging with consul key-value store
    GA-4662

--- guest-accounts-password-impl/src/main/java/com/rccl/middleware/guest/impl/GuestAccountModule.java
+++ guest-accounts-password-impl/src/main/java/com/rccl/middleware/guest/impl/GuestAccountModule.java
@@ -5,6 +5,7 @@ import com.lightbend.lagom.javadsl.server.ServiceGuiceSupport;
 import com.rccl.middleware.guest.impl.password.GuestAccountPasswordServiceImpl;
 import com.rccl.middleware.guest.password.GuestAccountPasswordService;
 import com.rccl.middleware.saviynt.api.SaviyntService;
+import com.rccl.ops.common.logging.LogLevelUpdateScheduler;
 
 public class GuestAccountModule extends AbstractModule implements ServiceGuiceSupport {
     
@@ -12,5 +13,6 @@ public class GuestAccountModule extends AbstractModule implements ServiceGuiceSu
     protected void configure() {
         bindService(GuestAccountPasswordService.class, GuestAccountPasswordServiceImpl.class);
         bindClient(SaviyntService.class);
+        bind(LogLevelUpdateScheduler.class).asEagerSingleton();
     }
 }
