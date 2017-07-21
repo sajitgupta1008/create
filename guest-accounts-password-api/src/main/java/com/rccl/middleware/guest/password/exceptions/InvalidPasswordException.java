0f9b1afab63b0718d01087acdf6cefdebf402c53
Nikko Mina <nmina@rccl.com>
2017-07-21 12:30:21 -0400

    Updated password exception message.

--- guest-accounts-password-api/src/main/java/com/rccl/middleware/guest/password/exceptions/InvalidPasswordException.java
+++ guest-accounts-password-api/src/main/java/com/rccl/middleware/guest/password/exceptions/InvalidPasswordException.java
@@ -7,6 +7,6 @@ public class InvalidPasswordException extends MiddlewareTransportException {
     
     public InvalidPasswordException() {
         super(TransportErrorCode.fromHttp(422), "The password must be between 8 and 32 characters, inclusive, " +
-                "with at least one (1) number.");
+                "with at least one (1) letter and one (1) number.");
     }
 }
