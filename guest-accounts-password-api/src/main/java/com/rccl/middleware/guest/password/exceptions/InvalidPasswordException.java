ee0b0211f65afbcd92da194ff550003471151828
Roberto Aleman Jr <raleman@rccl.com>
2017-08-23 17:45:41 -0400

    Fixed a checkstyle warning.

--- guest-accounts-password-api/src/main/java/com/rccl/middleware/guest/password/exceptions/InvalidPasswordException.java
+++ guest-accounts-password-api/src/main/java/com/rccl/middleware/guest/password/exceptions/InvalidPasswordException.java
@@ -6,7 +6,7 @@ import com.rccl.middleware.common.exceptions.MiddlewareTransportException;
 public class InvalidPasswordException extends MiddlewareTransportException {
     
     public InvalidPasswordException() {
-        super(TransportErrorCode.fromHttp(422), "The password must be between 8 and 32 characters, inclusive, " +
-                "with at least one (1) letter and one (1) number.");
+        super(TransportErrorCode.fromHttp(422), "The password must be between 8 and 32 characters, inclusive, "
+                + "with at least one (1) letter and one (1) number.");
     }
 }
