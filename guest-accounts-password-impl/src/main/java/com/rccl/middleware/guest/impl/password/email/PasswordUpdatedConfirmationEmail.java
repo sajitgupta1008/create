3968f6f7290748643470b7ee290f34914451d3f2
Roberto Aleman Jr <raleman@rccl.com>
2017-10-17 11:58:11 -0400

    Initial PasswordUpdatedConfirmationEmail commit.

---
+++ guest-accounts-password-impl/src/main/java/com/rccl/middleware/guest/impl/password/email/PasswordUpdatedConfirmationEmail.java
@@ -0,0 +1,67 @@
+package com.rccl.middleware.guest.impl.password.email;
+
+import ch.qos.logback.classic.Logger;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.lightbend.lagom.javadsl.api.transport.TransportErrorCode;
+import com.lightbend.lagom.javadsl.persistence.PersistentEntityRegistry;
+import com.rccl.middleware.aem.api.AemService;
+import com.rccl.middleware.common.exceptions.MiddlewareTransportException;
+import com.rccl.middleware.common.logging.RcclLoggerFactory;
+import com.rccl.middleware.guest.password.EmailNotification;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.concurrent.CompletionStage;
+
+public class PasswordUpdatedConfirmationEmail {
+    private static final Logger LOGGER = RcclLoggerFactory.getLogger(ResetPasswordEmail.class);
+    
+    private AemService aemService;
+    
+    private PersistentEntityRegistry persistentEntityRegistry;
+    
+    public PasswordUpdatedConfirmationEmail(AemService aemService,
+                              PersistentEntityRegistry persistentEntityRegistry) {
+        this.aemService = aemService;
+        this.persistentEntityRegistry = persistentEntityRegistry;
+    }
+    
+    public void send(String email) {
+        this.getPasswordUpdatedConfirmationEmailTemplate()
+                .thenAccept(aemTemplateResponse -> {
+                    String content = this.getPopulatedEmailTemplate(aemTemplateResponse, email);
+                    String sender = "notifications@rccl.com";
+                    String subject = aemTemplateResponse.get("subject").asText();
+                    
+                    EmailNotification en = EmailNotification.builder()
+                            .content(content)
+                            .recipient(email)
+                            .sender(sender)
+                            .subject(subject)
+                            .build();
+                    
+                    this.sendToTopic(en);
+                });
+    }
+    
+    private CompletionStage<JsonNode> getPasswordUpdatedConfirmationEmailTemplate() {
+        // TODO: Update to retrieve the password updated confirmation email template
+        // TODO: aemService.getPasswordUpdatedConfirmationEmailTemplate()
+        return aemService.getResetPasswordEmail()
+                .invoke()
+                .exceptionally(throwable -> {
+                    LOGGER.error("#getPasswordUpdatedConfirmationEmailTemplate:", throwable);
+                    throw new MiddlewareTransportException(TransportErrorCode.fromHttp(500), throwable);
+                });
+    }
+    
+    private String getPopulatedEmailTemplate(JsonNode aemTemplateResponse, String email) {
+        // TODO: Add logic for replacing values.
+        return null;
+    }
+    
+    private void sendToTopic(EmailNotification emailNotification) {
+        persistentEntityRegistry
+                .refFor(EmailNotificationEntity.class, emailNotification.getRecipient())
+                .ask(new EmailNotificationCommand.SendEmailNotification(emailNotification));
+    }
+}
