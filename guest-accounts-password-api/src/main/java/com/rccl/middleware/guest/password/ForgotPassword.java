ac72dae86474ba0173a759472fd1b1f37f455459
Roberto Aleman Jr <raleman@rccl.com>
2017-10-19 15:28:55 -0400

    Added a Header property.

--- guest-accounts-password-api/src/main/java/com/rccl/middleware/guest/password/ForgotPassword.java
+++ guest-accounts-password-api/src/main/java/com/rccl/middleware/guest/password/ForgotPassword.java
@@ -1,11 +1,14 @@
 package com.rccl.middleware.guest.password;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.rccl.middleware.common.header.Header;
 import com.rccl.middleware.common.validation.validator.ValidatorConstants;
 import lombok.Builder;
 import lombok.Value;
 import org.hibernate.validator.constraints.NotEmpty;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -13,6 +16,10 @@ import javax.validation.constraints.Size;
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ForgotPassword {
+    
+    @NotNull(message = "A header is required.")
+    @Valid
+    Header header;
     
     @NotEmpty(message = "An email is required.")
     @Size(min = 5, max = 100, message = "The email can have a minimum of 5 characters and "
