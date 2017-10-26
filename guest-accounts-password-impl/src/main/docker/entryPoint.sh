0ffc5f8e3d13dbd88f6a4aa2aa38bee719662a4a
Roberto Aleman Jr <raleman@rccl.com>
2017-10-26 10:49:40 -0400

    Fixed entryPoint script to remove unnecessary logic.

--- guest-accounts-password-impl/src/main/docker/entryPoint.sh
+++ guest-accounts-password-impl/src/main/docker/entryPoint.sh
@@ -3,19 +3,19 @@
 set -e
 
 echo "Entry Point"
+
 APP_LIB=/usr/src/app/lib
 APP_CLASSPATH=$APP_LIB/*
-APP_CLUSTER="guest-accounts-password-application"
-CINNAMON_JAR=/usr/src/app/lib/com.lightbend.cinnamon-cinnamon-agent-2.4.0.jar
-PLAY_SECRET=none
 
-#CONFIG="-Dplay.crypto.secret=$PLAY_SECRET -Dlagom.cluster.join-self=on"
-CONFIG="-Dplay.crypto.secret=$PLAY_SECRET -Dplay.akka.actor-system=$APP_CLUSTER"
-
-PLAY_SERVER_START="play.core.server.ProdServerStart"
 appid=$(echo $MESOS_TASK_ID |  cut -d. -f1)
 taskid=$(echo $MESOS_TASK_ID |  cut -d. -f2)
 export taskid
 export appid
 
-exec java -cp "$APP_CLASSPATH" $JAVA_OPTS $JMX_CONFIG -javaagent:$CINNAMON_JAR  $CONFIG $PLAY_SERVER_START
+JAVA_OPTS=""
+JMX_CONFIG=""
+PLAY_SECRET=none
+CONFIG="-Dplay.crypto.secret=$PLAY_SECRET -Dlagom.cluster.join-self=on"
+PLAY_SERVER_START="play.core.server.ProdServerStart"
+
+exec java -cp "$APP_CLASSPATH" $JAVA_OPTS $JMX_CONFIG $CONFIG $PLAY_SERVER_START
